28d02694099a9dc8af4b1f4ef3df482b3cc891c0
BidaoVIP <fabriciofagundes238@gmail.com>
2024-06-07 10:32:49 -0300

    criando models categoria e produto

---
+++ Models/Categoria.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CardapioWeb.Models
+{
+    [Table("Categorias")]
+    public class Categoria
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+    }
+}
