ad437a25c552240439f7e89ac7e0b8a7ea71064c
BidaoVIP <fabriciofagundes238@gmail.com>
2024-06-07 10:48:11 -0300

    declaração da classe categoria

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -10,6 +10,12 @@ namespace CardapioWeb.Models
         [Key]
         public int Id { get; set; }
         public string Nome { get; set; }
-        public string Descricao { get; set; }
+        public string Descricao_Curta { get; set; }
+        public string Descricao_Detalhada { get; set; }
+        public decimal Preco { get; set; }
+        public string Local_Tempo { get; set; }
+        public bool Produto_Preferido { get; set; }
+        public bool Em_Estoque { get; set; }
+
     }
 }
