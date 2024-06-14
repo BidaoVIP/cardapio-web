06a27d006fd7f2d1bc9a8695d4a95f8e2f73cd99
BidaoVIP <fabriciofagundes238@gmail.com>
2024-06-14 10:29:25 -0300

    Alterações na classe produto e categoria

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -9,13 +9,12 @@ namespace CardapioWeb.Models
     {
         [Key]
         public int Id { get; set; }
+        [Display(Name = "Nome da categoria")]
+        [Required(ErrorMessage = "Campo obrigatório")]
         public string Nome { get; set; }
-        public string Descricao_Curta { get; set; }
-        public string Descricao_Detalhada { get; set; }
-        public decimal Preco { get; set; }
-        public string Local_Tempo { get; set; }
-        public bool Produto_Preferido { get; set; }
-        public bool Em_Estoque { get; set; }
+        [Display(Name = "Descrição da categoria")]
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Descricao { get; set; }
 
     }
 }
