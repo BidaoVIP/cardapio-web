3c4671c9c27b7c69dffae81b694c5078858b9b96
BidaoVIP <fabriciofagundes238@gmail.com>
2024-06-21 10:40:59 -0300

    Criação das classes e das interfaces Produto e Categoria

---
+++ Repositories/CategoriaRepository.cs
@@ -0,0 +1,7 @@
+﻿
+namespace CardapioWeb.Repositories
+{
+    public class CategoriaRepository
+    {
+    }
+}
---
+++ Repositories/ICategoriaRepository.cs
@@ -0,0 +1,24 @@
+﻿using CardapioWeb.Models;
+
+namespace CardapioWeb.Repositories
+{
+    public interface ICategoriaRepository
+    {
+        IEnumerable<Categoria> GetAll();
+    }
+}
+         ________
+        /    |   \
+        |________|
+        |        |
+        |        |
+        |        |
+        |        |
+        |        |
+        |        |
+        |        |
+        |        |
+    ____|        |____
+   /                  \
+   |                  |
+   |_________|________|
